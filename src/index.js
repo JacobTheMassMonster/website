fb202fcda7628bef9f8d4e602cea026d4d74fa13
JacobTheProgrammer <jacobtheperson123@hotmail.com>
2018-09-06 21:24:41 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+/*
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+*/
+
+/*
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+*/
+
+class Welcome extends Component {
+	render() {
+		return (
+			<h1>
+				Hello, {this.props.name}
+			</h1>
+		);
+	}
+}
+
+function App2() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App2 />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
